bdfbac92df57e150bb6f32cc5424d5a87f6ba57c
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2022-10-12 10:14:32 -0300

    Importando tokens

--- Lab-Design-System/src/App.tsx
+++ Lab-Design-System/src/App.tsx
@@ -1,7 +1,13 @@
 import "./styles/global.css"
 export const App = () => {
   return (
+<<<<<<< HEAD
     <h1> hello word</h1>
+=======
+    <button className="bg-cyan-300 text-2xl">
+
+    </button>
+>>>>>>> configs
   )
 }
 
