0efcb92a71911e0feaf527e646a718b1d5e3e0e9
Evandro Rodrigues Coelho <evandrorodcoelho@gmail.com>
2022-10-13 21:09:21 -0300

    Alterando storiesbook

--- src/components/Checkbox.stories.tsx
+++ src/components/Checkbox.stories.tsx
@@ -2,13 +2,14 @@ import { Checkbox, CheckboxProps } from "./Checkbox";
 import { Meta, StoryObj } from "@storybook/react"
 import { Text } from "./Text";
 export default {
-    title: 'Components/Button',
+    title: 'Components/Checkbox',
     component: Checkbox,
     args: {},
     argTypes: {},
     decorators: [(Story) => {
         return (
             <div className="flex items-center gap-2">
+                {Story()}
                 <Text size="sm">Lembrar de mim durante 30 dias</Text>
             </div>
         )
--- src/components/text.stories.tsx
+++ src/components/text.stories.tsx
@@ -1,10 +1,33 @@
 import { Component } from "react";
-import { Text } from "./Text";
-
+import { Text, TextProps } from "./Text";
+import { Meta, StoryObj } from "@storybook/react";
 
 export default {
     title: 'components/text',
-    component: Text
-}
+    component: Text,
+    args: {
+        children: "Lorem ipsum",
+        size: "md"
+    },
+    argTypes: {
+        size: {
+            options: ["sm", "md", "lg"],
+            control: {
+                type: "inline-radio"
+            }
+        }
+    }
+} as Meta<TextProps>
 
-export const Default = {}
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size: "sm"
+    }
+}
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size: "lg"
+    }
+}
